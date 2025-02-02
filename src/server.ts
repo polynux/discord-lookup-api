844b45334a262fa64ebe88959cf3eaae60a68e04
Guillaume Dorce <polynux.dev@gmail.com>
2025-02-02 19:42:31 +0100

    use tsx to dev

--- src/server.ts
+++ src/server.ts
@@ -25,7 +25,7 @@ app.use((req: Request, res: Response, next: NextFunction) => {
     next();
 });
 
-app.use((req: Request, res: Response, next) => {
+app.use((req: Request, res: Response, next: NextFunction) => {
     req.disableCache = disableCache;
     req.redisClient = redisClient;
     next();
--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -1,3 +1,4 @@
+/// /// <reference path="@types/express" />
 import RedisWrapper from "../../redisClient";
 
 export {};
